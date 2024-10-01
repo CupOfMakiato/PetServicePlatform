ef25176a28858225e1fcfef87e832eea1188192f
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2024-10-01 14:02:39 +0700

    create branch Kiet

--- Server.API/obj/Debug/net8.0/Server.API.AssemblyInfo.cs
+++ Server.API/obj/Debug/net8.0/Server.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cef1bf01a874c41a16af0fc13b33cfb5940e5c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
